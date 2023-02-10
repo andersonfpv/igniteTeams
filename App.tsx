b139bea0534f58ff0bf42c8185fe9fa199723d6f
Anderson Felipe Pereira Vieira <anderson.vieira@a12.com>
2023-02-10 14:54:23 -0300

    Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
@@ -9,6 +10,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   );
