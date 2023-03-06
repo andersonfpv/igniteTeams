0338eaf86224de5c5e4f1bcc936a0e5b02d94da4
Anderson Felipe Pereira Vieira <anderson.vieira@a12.com>
2023-03-06 11:27:17 -0300

    Passando parâmetros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+
+import { Container, Content, Icon } from "./styles";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container, Content, Icon } from "./styles";
 
 export function NewGroup(){
+    const [group, setGroup] = useState('');
+
     const navigation = useNavigation();
 
     function handleNew(){
-        navigation.navigate('players', { group: 'Rocket' });
+        navigation.navigate('players', { group });
     }
 
     return (
@@ -22,7 +27,8 @@ export function NewGroup(){
                     subtitle="crie a turma para adicionar as pessoas"
                 />
                 <Input 
-                placeholder="Nome da turma"
+                    placeholder="Nome da turma"
+                    onChangeText={setGroup}
                 />
 
                 <Button 
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useRoute } from "@react-navigation/native";
 import { FlatList } from "react-native";
 
 import { Input } from "@components/Input";
@@ -12,15 +13,23 @@ import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
+type RouteParams = {
+    group: string;
+}
+
 export function Players() {
     const [team, setTeam] = useState('Time A');
     const [players, setPlayers] = useState([]);
+
+    const route = useRoute();
+    const { group } = route.params as RouteParams;
+
     return (
         <Container>
             <Header showBackButton />
 
             <Highlight 
-                title="Nome da turma"
+                title={group}
                 subtitle="adicione a galera e separe os times"
             />
 
